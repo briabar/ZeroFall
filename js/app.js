f03580eac3689a7d2d0897c26257911b547bac6f
brianbar <brianbaron@gmail.com>
2022-03-22 12:10:27 -0700

    old repo erases, accidentally checked in key... bullets fall, typing can be read

---
+++ js/app.js
@@ -0,0 +1,153 @@
+
+//Set up state variables
+let gameEnded = false;
+const l1Nodes = document.querySelectorAll('#l1 > div');
+const l2Nodes = document.querySelectorAll('#l2 > div');
+const l3Nodes = document.querySelectorAll('#l3 > div');
+const l4Nodes = document.querySelectorAll('#l4 > div');
+const l5Nodes = document.querySelectorAll('#l5 > div');
+const l6Nodes = document.querySelectorAll('#l6 > div');
+const l7Nodes = document.querySelectorAll('#l7 > div');
+const l8Nodes = document.querySelectorAll('#l8 > div');
+const deadNodes = document.querySelectorAll('.dead');
+const laneList = [l1Nodes, l2Nodes, l3Nodes, l4Nodes, l5Nodes, l6Nodes, l7Nodes, l8Nodes];
+const lane1 = document.querySelector('#l1');
+const cityQuestionArea = document.querySelector('#city');
+const userAnswerScreen = document.querySelector('#UI');
+let userInput = [];
+
+const bombList = [];
+const bombDetails = {
+    0: [
+
+    ],
+
+}
+
+const timer = ms => new Promise(res => setTimeout(res, ms)); //master timer
+let gameLevel = 0; // which level you're on... 0 = 1, 1 = 2...
+
+userAnswerScreen.addEventListener('keyup', (e)=> {
+    console.log(e.code);
+});
+
+userAnswerScreen.addEventListener('blur', (e)=> {
+    userAnswerScreen.focus();
+});
+//keep focus on user screen
+userAnswerScreen.focus();
+
+
+
+class Bomb {
+    constructor(word, color, question, answer, lane) {
+        this.word = word;
+        this.color = color;
+        this.question = question;
+        this.answer = answer;
+        this.lane = lane;
+        this.location = 0;
+        let el = document.createElement('div');
+        el.classList.add('bomb');
+        el.style.color = this.color;
+        el.innerText = this.word;
+        this.element = el;
+        this.lane[this.location].appendChild(this.element);
+    }
+    moveDown() {
+        this.location += 1;
+        this.lane[this.location].appendChild(this.element);
+    }
+}
+
+
+
+// let bomb1 = new Bomb('green', 'green', 'green', l1Nodes);
+
+// let levelNum = 0;
+// const challenges = {
+//     0 : ["question 1"],
+//     1 : ["question 2"],
+//     //... and so on...
+// }
+
+// const bombsAdded = [];
+// let winCondition = 0;
+// let userChallengeEl = "bind me to the user input area and enter key"
+
+
+
+
+// function init(level) {
+//     //set start on spacebar
+//     //clear board
+//     //populate questions array (obj?)
+//     //win condition = challenges[0].length
+
+// }
+
+// //controller
+
+
+
+
+
+const controller = {
+    generateRandomLane: function() {
+        return laneList[Math.floor(Math.random() * 8)];
+    },
+
+    getBombDeets(lev) {
+        let bombIngredients = {
+            word: 'green', //take randomly from bomb details
+            color: 'red',
+            question: 'type the color',
+            answer: 'red',
+        }
+        return bombIngredients;
+    },
+
+    generateNewBomb: function(level) {
+        let bombDeets = controller.getBombDeets(level);
+        let newBomb = new Bomb(bombDeets.word,bombDeets.color,bombDeets.question, bombDeets.answer, controller.generateRandomLane());
+        bombList.push(newBomb);
+    },
+
+    checkDead: function() {
+        deadNodes.forEach(element => {
+            if (element.children.length > 0) {
+                gameEnded = true;
+            }
+        });
+    }
+
+}
+
+const view = {
+    moveBombsDown: function() {
+        bombList.forEach((bomb) => {
+            bomb.moveDown()
+        });
+
+    },
+}
+
+// //while not lost ....
+//     init(levelNum); // set up level
+//     running()
+async function running () {
+    let staggerBombs = 0;
+    while (!gameEnded) {
+        if (staggerBombs === 60) {
+            staggerBombs = 0;
+            controller.generateNewBomb(gameLevel);
+        }
+        view.moveBombsDown();
+        controller.checkDead();
+        staggerBombs += 1
+        await timer(50);
+    }
+    console.log("game over");
+}
+console.log(lane1.children.length)
+running()
---
+++ userstory.md
@@ -0,0 +1,43 @@
+# User Story
+
+## Part The First: Jack
+
+Jack was a hardworker, but he was all work and no play.  One day after helping his friend Jill up a nearby hill he decided that he should really relax.  Unfortunately, this led him to a game called el pescado es espectacular, a game that has nothing to do with fish.
+
+## The Game
+After sitting down, Jack is greeted by a game screen on it is an azure sky with billowing clouds and below it a city unlike any other.
+
+Under the city is a black box that simply says "hit spacebar to begin".  He does so. Gently.
+
+Bombs begin to fall towards the city! Oh no!  Fortunately he has a way to fight off the bombs!
+
+"type the word RED," suggests the black box.
+
+He does so and the bomb is destroyed.  Though now it looks like more are coming...  To be continued.
+
+
+## Gameplay Concept
+
+The idea of the game is to create create ever more confusing user actions.  In addition, the speed of the bombs will also increase over time.  Each level will add onto the last.
+
+### Level One
+
+Simple commands and slow bombs
+
+### level two
+
+type the color of a word / type the word of a color.  
+
+### level three
+
+Type nothing. / a single bomb falls.
+
+### level four
+Don't type the letter 'p'
+    - type pizze
+    - type pepperoni
+    - type peppa pig 
+    - don't pepperocini
+
+### level five
+Type nothing.  must type "Nothing", after that more bombs will fall.
