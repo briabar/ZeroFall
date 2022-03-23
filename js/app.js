f4feca587740271516f3a4e70456ed297aa5e05b
brianbar <brianbaron@gmail.com>
2022-03-22 23:34:56 -0700

    MVP complete

--- js/app.js
+++ js/app.js
@@ -9,34 +9,32 @@ const l5Nodes = document.querySelectorAll('#l5 > div');
 const l6Nodes = document.querySelectorAll('#l6 > div');
 const l7Nodes = document.querySelectorAll('#l7 > div');
 const l8Nodes = document.querySelectorAll('#l8 > div');
-const deadNodes = document.querySelectorAll('.dead');
+let isDead;
 const laneList = [l1Nodes, l2Nodes, l3Nodes, l4Nodes, l5Nodes, l6Nodes, l7Nodes, l8Nodes];
 const lane1 = document.querySelector('#l1');
-const cityQuestionArea = document.querySelector('#city');
+const cityQuestionArea = document.querySelector('#question');
 const userAnswerScreen = document.querySelector('#UI');
 let userInput = [];
 
 const bombList = [];
 const bombDetails = {
-    0: [
-
-    ],
+    0: [{
+        word: 'red',
+        color: 'green',
+        question: "type the color",
+        answer: 'green',
+    },
+    {
+        word: 'green',
+        color: 'blue',
+        question: "type the word",
+        answer: 'green',
+    },],
 
 }
 
 const timer = ms => new Promise(res => setTimeout(res, ms)); //master timer
 let gameLevel = 0; // which level you're on... 0 = 1, 1 = 2...
-
-userAnswerScreen.addEventListener('keyup', (e)=> {
-    console.log(e.code);
-});
-//keep focus on user screen
-userAnswerScreen.addEventListener('blur', (e)=> {
-    userAnswerScreen.focus();
-});
-
-userAnswerScreen.focus();
-
 
 
 class Bomb {
@@ -55,23 +53,35 @@ class Bomb {
         this.lane[this.location].appendChild(this.element);
     }
     moveDown() {
-        this.location += 1;
-        this.lane[this.location].appendChild(this.element);
+        if (!this.element.classList.contains('.destroyed')) {
+            this.location += 1;
+            this.element.style.top = `${this.location/4}%`;
+        }
     }
 }
 
 
 const controller = {
+    grabUserInput: function() {
+        if (bombList[0]) {
+            if(userAnswerScreen.value === bombList[0].answer) {
+                view.removeBomb();
+                userAnswerScreen.value = "";
+            }
+        }
+    },
+
     generateRandomLane: function() {
         return laneList[Math.floor(Math.random() * 8)];
     },
 
     getBombDeets(lev) {
+        let fromBombDetails = bombDetails[lev][Math.floor(Math.random() * bombDetails[lev].length)];
         let bombIngredients = {
-            word: 'green', //take randomly from bomb details
-            color: 'red',
-            question: 'type the color',
-            answer: 'red',
+            word: fromBombDetails.word, //take randomly from bomb details
+            color: fromBombDetails.color,
+            question: fromBombDetails.question,
+            answer: fromBombDetails.answer,
         }
         return bombIngredients;
     },
@@ -82,13 +92,13 @@ const controller = {
         bombList.push(newBomb);
     },
 
-    checkDead: function() {
-        deadNodes.forEach(element => {
-            if (element.children.length > 0) {
-                gameEnded = true;
-            }
-        });
-    }
+    // checkDead: function(isDead) {
+    //     deadNodes.forEach(element => {
+    //         if (element.children.length > 0) {
+    //             gameEnded = true;
+    //         }
+    //     });
+    // }
 
 }
 
@@ -96,27 +106,65 @@ const view = {
     moveBombsDown: function() {
         bombList.forEach((bomb) => {
             bomb.moveDown()
+            if (parseFloat(bomb.element.style.top) >= 79) {
+                gameEnded = true;
+            }
         });
 
     },
+    removeBomb: function() {
+        let bombToRemove = bombList.shift();
+        bombToRemove.element.remove();
+        view.showQuestion()
+    },
+    showQuestion: function() {
+        if (bombList[0]) {
+            if (bombList[0].question != cityQuestionArea.innerText) {
+                cityQuestionArea.display = 'none';
+                cityQuestionArea.innerText = bombList[0].question;
+                cityQuestionArea.display = 'inline';
+            }
+        }
+    }
 }
 
 // //while not lost ....
 //     init(levelNum); // set up level
 //     running()
-async function running () {
-    let staggerBombs = 0;
-    while (!gameEnded) {
-        if (staggerBombs === 60) {
+
+//hookup UI
+userAnswerScreen.addEventListener('keyup', (keyStroke) => {
+    controller.grabUserInput(keyStroke);
+    });
+//keep focus on user screen
+userAnswerScreen.addEventListener('blur', ()=> {
+    userAnswerScreen.focus();
+});
+
+userAnswerScreen.focus();
+
+
+let staggerBombs = 15;
+
+function running () {
+    if (!gameEnded) {
+        if (staggerBombs === 25) {
             staggerBombs = 0;
             controller.generateNewBomb(gameLevel);
+
+            if (bombList[0].question !== cityQuestionArea.innerText) {
+                view.showQuestion()
+            }
         }
         view.moveBombsDown();
-        controller.checkDead();
         staggerBombs += 1
-        await timer(50);
     }
-    console.log("game over");
+    else {
+        console.log("game over");
+        clearInterval(intervalID);
+    }
+
 }
 console.log(lane1.children.length)
-running()
+// running()
+var intervalID = setInterval(running, 30);
