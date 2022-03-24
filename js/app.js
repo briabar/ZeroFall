9970cf743fb671bbe2fb1de2787795a9def81d79
brianbar <brianbaron@gmail.com>
2022-03-23 23:08:27 -0700

    got styling finished, begin adding levels data, added spacebar

--- js/app.js
+++ js/app.js
@@ -1,6 +1,513 @@
 
-//Set up state variables
-let gameEnded = false;
+//Set up state variables first are a few super long ones...
+wordList = [
+    "the",
+    "and",
+    "to",
+    "of",
+    "a",
+    "in",
+    "is",
+    "that",
+    "for",
+    "I",
+    "you",
+    "it",
+    "with",
+    "on",
+    "as",
+    "are",
+    "be",
+    "this",
+    "was",
+    "have",
+    "or",
+    "at",
+    "not",
+    "your",
+    "from",
+    "we",
+    "by",
+    "will",
+    "can",
+    "but",
+    "they",
+    "an",
+    "he",
+    "all",
+    "has",
+    "if",
+    "their",
+    "one",
+    "do",
+    "more",
+    "nt",
+    "my",
+    "his",
+    "so",
+    "there",
+    "about",
+    "which",
+    "when",
+    "what",
+    "out",
+    "up",
+    "our",
+    "who",
+    "also",
+    "had",
+    "time",
+    "some",
+    "would",
+    "were",
+    "like",
+    "been",
+    "just",
+    "her",
+    "new",
+    "other",
+    "them",
+    "she",
+    "people",
+    "these",
+    "no",
+    "get",
+    "how",
+    "me",
+    "into",
+    "than",
+    "only",
+    "its",
+    "most",
+    "may",
+    "any",
+    "many",
+    "make",
+    "then",
+    "well",
+    "first",
+    "very",
+    "over",
+    "now",
+    "could",
+    "after",
+    "even",
+    "because",
+    "us",
+    "said",
+    "good",
+    "way",
+    "two",
+    "should",
+    "work",
+    "use",
+    "through",
+    "see",
+    "know",
+    "did",
+    "much",
+    "where",
+    "years",
+    "need",
+    "him",
+    "back",
+    "such",
+    "those",
+    "being",
+    "day",
+    "take",
+    "while",
+    "here",
+    "before",
+    "does",
+    "great",
+    "year",
+    "go",
+    "help",
+    "want",
+    "really",
+    "think",
+    "best",
+    "life",
+    "each",
+    "made",
+    "right",
+    "world",
+    "business",
+    "home",
+    "own",
+    "down",
+    "still",
+    "used",
+    "find",
+    "around",
+    "going",
+    "every",
+    "both",
+    "last",
+    "off",
+    "too",
+    "same",
+    "information",
+    "little",
+    "another",
+    "look",
+    "few",
+    "long",
+    "part",
+    "since",
+    "things",
+    "place",
+    "am",
+    "between",
+    "during",
+    "different",
+    "must",
+    "come",
+    "using",
+    "however",
+    "without",
+    "high",
+    "why",
+    "something",
+    "online",
+    "system",
+    "better",
+    "three",
+    "never",
+    "always",
+    "love",
+    "say",
+    "might",
+    "next",
+    "company",
+    "state",
+    "number",
+    "again",
+    "free",
+    "lot",
+    "under",
+    "family",
+    "found",
+    "within",
+    "give",
+    "set",
+    "school",
+    "important",
+    "water",
+    "able",
+    "keep",
+    "got",
+    "sure",
+    "end",
+    "money",
+    "service",
+    "small",
+    "put",
+    "experience",
+    "having",
+    "once",
+    "available",
+    "health",
+    "support",
+    "often",
+    "including",
+    "days",
+    "away",
+    "old",
+    "area",
+    "feel",
+    "read",
+    "show",
+    "big",
+    "against",
+    "thing",
+    "order",
+    "program",
+    "though",
+    "city",
+    "group",
+    "services",
+    "site",
+    "making",
+    "course",
+    "point",
+    "children",
+    "times",
+    "team",
+    "game",
+    "along",
+    "let",
+    "house",
+    "today",
+    "body",
+    "working",
+    "case",
+    "man",
+    "real",
+    "provide",
+    "care",
+    "public",
+    "top",
+    "looking",
+    "several",
+    "start",
+    "less",
+    "process",
+    "become",
+    "actually",
+    "local",
+    "together",
+    "person",
+    "change",
+    "book",
+    "enough",
+    "getting",
+    "week",
+    "power",
+    "until",
+    "market",
+    "fact",
+    "god",
+    "food",
+    "students",
+    "full",
+    "women",
+    "community",
+    "name",
+    "second",
+    "data",
+    "government",
+    "says",
+    "others",
+    "ever",
+    "yet",
+    "research",
+    "done",
+    "left",
+    "far",
+    "large",
+    "called",
+    "doing",
+    "already",
+    "development",
+    "social",
+    "open",
+    "possible",
+    "side",
+    "play",
+    "means",
+    "needs",
+    "try",
+    "came",
+    "ca",
+    "based",
+    "hard",
+    "thought",
+    "products",
+    "national",
+    "quality",
+    "level",
+    "live",
+    "design",
+    "makes",
+    "project",
+    "line",
+    "night",
+    "least",
+    "whether",
+    "job",
+    "car",
+    "example",
+    "include",
+    "following",
+    "given",
+    "website",
+    "past",
+    "plan",
+    "offer",
+    "buy",
+    "call",
+    "went",
+    "simply",
+    "hand",
+    "music",
+    "easy",
+    "problem",
+    "men",
+    "country",
+    "took",
+    "four",
+    "members",
+    "form",
+    "personal",
+    "control",
+    "energy",
+    "room",
+    "head",
+    "pay",
+    "create",
+    "run",
+    "kind",
+    "credit",
+    "almost",
+    "believe",
+    "quite",
+    "mind",
+    "law",
+    "early",
+    "comes",
+    "states",
+    "usually",
+    "companies",
+    "web",
+    "taking",
+    "started",
+    "later",
+    "although",
+    "story",
+    "per",
+    "future",
+    "known",
+    "someone",
+    "across",
+    "rather",
+    "young",
+    "whole",
+    "special",
+    "everything",
+    "months",
+    "anything",
+    "training",
+    "url",
+    "bit",
+    "seen",
+    "product",
+    "american",
+    "please",
+    "management",
+    "cost",
+    "either",
+    "light",
+    "university",
+    "face",
+    "due",
+    "nothing",
+    "human",
+    "event",
+    "history",
+    "probably",
+    "friends",
+    "learn",
+    "current",
+    "tell",
+    "general",
+    "price",
+    "list",
+    "type",
+    "building",
+    "industry",
+    "bad",
+    "check",
+    "everyone",
+    "office",
+    "idea",
+    "internet",
+    "news",
+    "million",
+    "video",
+    "among",
+    "air",
+    "especially",
+    "told",
+    "results",
+    "post",
+    "hours",
+    "international",
+    "center",
+    "understand",
+    "above",
+    "addition",
+    "major",
+    "education",
+    "white",
+    "particular",
+    "problems",
+    "media",
+    "according",
+    "upon",
+    "page",
+    "continue",
+    "black",
+    "study",
+    "issues",
+    "inside",
+    "technology",
+    "five",
+    "value",
+    "further",
+    "access",
+    "reason",
+    "short",
+    "true",
+    "simple",
+    "natural",
+    "amount",
+    "search",
+    "result",
+    "taken",
+    "main",
+    "heart",
+    "space",
+    "financial",
+    "ago",
+    "trying",
+    "question",
+    "living",
+    "likely",
+    "interest",
+    "various",
+    "insurance",
+    "common",
+    "move",
+    "child",
+    "yourself",
+    "report",
+    "certain",
+    "share",
+    "single",
+    "close",
+    "instead",
+    "bring",
+    "works",
+    "age",
+    "s",
+    "season",
+    "hope",
+    "coming",
+    "areas",
+    "ask",
+    "medical",
+    "low",
+    "games",
+    "turn",
+    "key",
+    "party",
+    "add",
+    "month",
+    "seems",
+    "view",
+    "fun",
+    "matter",
+    "words",
+    "needed"];
+    
+colorList = [
+    'green', 'red', 'blue', 'yellow', 'black', 'orange', 'pink', 'purple'
+];
+
+
+
 const l1Nodes = document.querySelectorAll('#l1 > div');
 const l2Nodes = document.querySelectorAll('#l2 > div');
 const l3Nodes = document.querySelectorAll('#l3 > div');
@@ -9,28 +516,53 @@ const l5Nodes = document.querySelectorAll('#l5 > div');
 const l6Nodes = document.querySelectorAll('#l6 > div');
 const l7Nodes = document.querySelectorAll('#l7 > div');
 const l8Nodes = document.querySelectorAll('#l8 > div');
+const body = document.querySelector('body');
+const spaceBarEl = document.createElement('div');
+spaceBarEl.innerText = "HIT SPACEBAR TO START";
+spaceBarEl.style.position = 'fixed';
+spaceBarEl.style.top = '42%';
+spaceBarEl.style.left = '38%';
+body.appendChild(spaceBarEl);
+
 let isDead;
 const laneList = [l1Nodes, l2Nodes, l3Nodes, l4Nodes, l5Nodes, l6Nodes, l7Nodes, l8Nodes];
 const lane1 = document.querySelector('#l1');
 const cityQuestionArea = document.querySelector('#question');
 const userAnswerScreen = document.querySelector('#UI');
 let userInput = [];
+let gameEnded = false;
+let numberOfBombs = 10;
+const startSpeed = 80;
+let speed = startSpeed;
 
-const bombList = [];
-const bombDetails = {
-    0: [{
-        word: 'red',
-        color: 'green',
-        question: "type the color",
-        answer: 'green',
+let bombList = [];
+const bombFunctions = [
+    () => { // only words
+        console.log("FUCK");
+        let randWord = wordList[Math.floor(Math.random() * (wordList.length))];
+        return {
+            word: randWord,
+            color: colorList[Math.floor(Math.random() * (colorList.length))],
+            question: "Type the word",
+            answer: randWord,
+        }
     },
-    {
-        word: 'green',
-        color: 'blue',
-        question: "type the word",
-        answer: 'green',
-    },],
+    () => {
+        let randColor = colorList[Math.floor(Math.random() * (colorList.length))];
+        return {
+            word: colorList[Math.floor(Math.random() * (colorList.length))],
+            color: randColor,
+            question: "Type the color",
+            answer: randColor,
+        }
+    },
+    //() => {}
+]
 
+const bombDetails = function(level) {
+    let funcIndex = Math.floor(Math.random() * (level + 1));
+    if (level > 1) {funcIndex = 1} // just two levels, lol
+    return bombFunctions[funcIndex]();
 }
 
 const timer = ms => new Promise(res => setTimeout(res, ms)); //master timer
@@ -62,11 +594,18 @@ class Bomb {
 
 
 const controller = {
-    grabUserInput: function() {
+    grabUserInput: function(keyStroke) {
         if (bombList[0]) {
             if(userAnswerScreen.value === bombList[0].answer) {
                 view.removeBomb();
                 userAnswerScreen.value = "";
+            }
+
+        }
+        if (spaceBarEl.style.display !== 'none') {
+            if (keyStroke.code === 'Space') {
+                spaceBarEl.style.display = 'none';
+                
             }
         }
     },
@@ -75,34 +614,52 @@ const controller = {
         return laneList[Math.floor(Math.random() * 8)];
     },
 
-    getBombDeets(lev) {
-        let fromBombDetails = bombDetails[lev][Math.floor(Math.random() * bombDetails[lev].length)];
-        let bombIngredients = {
+    generateNewBomb: function(level) {
+        let fromBombDetails = bombDetails(level);
+        let bombDeets = {
             word: fromBombDetails.word, //take randomly from bomb details
             color: fromBombDetails.color,
             question: fromBombDetails.question,
             answer: fromBombDetails.answer,
         }
-        return bombIngredients;
-    },
-
-    generateNewBomb: function(level) {
-        let bombDeets = controller.getBombDeets(level);
         let newBomb = new Bomb(bombDeets.word,bombDeets.color,bombDeets.question, bombDeets.answer, controller.generateRandomLane());
         bombList.push(newBomb);
     },
 
-    // checkDead: function(isDead) {
-    //     deadNodes.forEach(element => {
-    //         if (element.children.length > 0) {
-    //             gameEnded = true;
-    //         }
-    //     });
-    // }
+    increaseBombNumber: function(increaseNum) {
+        numberOfBombs += increaseNum;
+    },
 
+    nextLevel: function() {
+        view.clearBombs();
+        view.clearScreen();
+        gameLevel += 1;
+        if (speed > 10) {
+            speed -= 10
+        }
+        controller.increaseBombNumber(10);
+        clearInterval(intervalID);
+        intervalID = setInterval(running, speed);
+
+    },
+    hitSpacebar: function() {
+
+    }
 }
 
 const view = {
+    clearBombs: function() {
+        bombList.forEach((bomb) => {
+            bomb.element.remove();
+        });
+        bombList = [];
+    },
+
+    clearScreen: function() {
+        userAnswerScreen.value = '';
+        cityQuestionArea.innerText = '';
+    },
+
     moveBombsDown: function() {
         bombList.forEach((bomb) => {
             bomb.moveDown()
@@ -115,6 +672,7 @@ const view = {
     removeBomb: function() {
         let bombToRemove = bombList.shift();
         bombToRemove.element.remove();
+        numberOfBombs -= 1;
         view.showQuestion()
     },
     showQuestion: function() {
@@ -125,13 +683,24 @@ const view = {
                 cityQuestionArea.display = 'inline';
             }
         }
-    }
+    },
+
+    makeFlash: function() {
+        if (bombList[0]) {
+            if (bombList[0].element.style.borderColor !== 'yellow') {
+            bombList[0].element.style.borderColor = 'yellow';
+            }
+            else {
+                bombList[0].element.style.borderColor = 'white';
+            }
+        }
+
+    },
 }
 
-// //while not lost ....
-//     init(levelNum); // set up level
-//     running()
 
+
+userAnswerScreen.focus();
 //hookup UI
 userAnswerScreen.addEventListener('keyup', (keyStroke) => {
     controller.grabUserInput(keyStroke);
@@ -139,32 +708,54 @@ userAnswerScreen.addEventListener('keyup', (keyStroke) => {
 //keep focus on user screen
 userAnswerScreen.addEventListener('blur', ()=> {
     userAnswerScreen.focus();
-});
-
-userAnswerScreen.focus();
-
+    });
 
 let staggerBombs = 15;
 
-function running () {
-    if (!gameEnded) {
-        if (staggerBombs === 25) {
-            staggerBombs = 0;
-            controller.generateNewBomb(gameLevel);
+// function waiting () {
+//     if ( spaceBarEl.display != 'none') {
+//         //do nothing
+//     }
+//     else {
+//         clearInterval(pause)
+//         var intervalID = setInterval(running, speed);
+//     }
+// }
 
-            if (bombList[0].question !== cityQuestionArea.innerText) {
-                view.showQuestion()
+function running () {
+
+    if (!gameEnded) {
+        if (numberOfBombs >= 1) {
+            if (spaceBarEl.style.display === 'none') {
+                if (staggerBombs === 25) {
+                    staggerBombs = 0;
+                    controller.generateNewBomb(gameLevel);
+        
+                    if (bombList[0].question !== cityQuestionArea.innerText) {
+                        view.showQuestion()
+                    }
+                }
+                view.moveBombsDown();
+                staggerBombs += 1
             }
         }
-        view.moveBombsDown();
-        staggerBombs += 1
+        else {
+            controller.nextLevel()
+        }
     }
     else {
-        console.log("game over");
+        spaceBarEl.style.display = 'block';
+        view.clearBombs();
+        view.clearScreen();
         clearInterval(intervalID);
+        speed = startSpeed;
+        intervalID = setInterval(running, speed);
+        gameEnded = false;
     }
 
 }
 console.log(lane1.children.length)
 // running()
-var intervalID = setInterval(running, 30);
+
+let intervalID = setInterval(running, speed);
+const flash = setInterval(view.makeFlash, 500);
